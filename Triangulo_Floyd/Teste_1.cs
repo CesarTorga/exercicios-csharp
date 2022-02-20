5b997d2de13516700d0eeb716fbc4f7798f3391b
CesarTorga <cesartorga@gmail.com>
2022-02-20 18:15:04 -0300

    first commit

---
+++ Triangulo_Floyd/Teste_1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Triangulo_Floyd
+{
+    class Teste_1
+    {
+        static void Main(string[] args)
+
+        {
+            Console.WriteLine("Digite um número:");
+            int quant_degraus = int.Parse(Console.ReadLine());
+            Console.WriteLine("");
+            int i, j = 1;
+
+            for (i = 1; i <= quant_degraus; i++)
+            {
+                for (j = 1; j < (i + 1); j++)
+                {
+                    Console.Write("*" +" ");
+                }
+
+                Console.Write("\n");    
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ aplicacoes_csharp/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace aplicacoes_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno[] alunos = new Aluno[5];
+            var i_aluno = 0;
+            string opcaoUsuario = ObterOpcaoUsuario();
+
+            while (opcaoUsuario.ToUpper() != "X")
+            {
+                switch (opcaoUsuario)
+                {
+                    case "1":
+                        Console.WriteLine("Informe o nome do aluno:");
+                        var aluno = new Aluno();
+                        aluno.Nome = Console.ReadLine();
+
+                        Console.WriteLine("Informe a nota do aluno:");
+                        //Convertendo o string para decimal
+                        if (decimal.TryParse(Console.ReadLine(), out decimal nota))
+                        {
+                            aluno.Nota = nota;
+                        }
+                        else
+                        {
+                            throw new ArgumentException("Valor da nota deve ser deecimal");
+                        }
+
+                        alunos[i_aluno] = aluno;
+                        i_aluno++;
+                        
+                        break;
+                    case "2":
+                        foreach (var a in alunos)
+                        {
+                            if (!string.IsNullOrEmpty(a.Nome))
+                            {
+                                Console.WriteLine($"Aluno: {a.Nome} - Nota: {a.Nota}");
+                            }
+                        }
+
+                        break;
+                    case "3":
+                        decimal nota_total = 0;
+                        var numero_alunos = 0;
+
+                        for (int i =0; i < alunos.Length; i++)
+                        {
+                            if(!string.IsNullOrEmpty(alunos[i].Nome))
+                            {
+                                nota_total = nota_total + alunos[i].Nota;
+                                numero_alunos++;
+                            }
+                        }
+
+                        var media_notas = nota_total/numero_alunos;
+                        Console.WriteLine($"A média das notas dos alunos cadastrados foi de {media_notas}");
+
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+
+                opcaoUsuario = ObterOpcaoUsuario();
+            }
+        }
+
+        private static string ObterOpcaoUsuario()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Informe a opção desejada:");
+            Console.WriteLine("1 - Inserir novo aluno");
+            Console.WriteLine("2 - Listar alunos");
+            Console.WriteLine("3 - Calcular média geral das notas");
+            Console.WriteLine("X - Sair do menu");
+            Console.WriteLine();
+
+            string opcaoUsuario = Console.ReadLine();
+            Console.WriteLine();
+            return opcaoUsuario;
+        }
+    }
+}
