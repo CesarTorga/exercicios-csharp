b11fb8d6d82945ef5bd845d9df5cf80a5648ed5a
César Torga <99417307+CesarTorga@users.noreply.github.com>
2022-02-21 17:57:36 -0300

    Add files via upload

---
+++ Ninja.cs
@@ -0,0 +1,22 @@
+namespace RPG.Entities
+{
+    public class Ninja : Hero
+    {
+        public Ninja(string Name, int Level, string HeroType, int HP, int MP)
+        {
+            this.Name = Name;
+            this.Level = Level;
+            this.HeroType = HeroType;
+            this.HP = HP;
+            this.MP = MP;
+        }
+        public override string Attack()
+            {
+                return this.Name + " Atacou com sua Águia gigante";
+            }
+        public string Attack(int Bonus)
+        {
+            return this.Name + " Explodiu o inimigo com mais " + Bonus;
+        }
+    }
+}
